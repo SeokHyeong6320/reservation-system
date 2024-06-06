797fbdeeaad1f1f981013722e4b4fdf2de0b916b
SeokHyeong6320 <hsh9171@icloud.com>
2024-06-06 21:30:44 +0900

    feat: [025-REFACTOR-KIOSK-SERVICE]
    kiosk-service 리팩토링

--- kiosk-service/src/main/java/com/project/kioskservice/KioskServiceApplication.java
+++ kiosk-service/src/main/java/com/project/kioskservice/KioskServiceApplication.java
@@ -2,10 +2,16 @@ package com.project.kioskservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EntityScan("com.project")
+@EnableJpaAuditing
+@EnableJpaRepositories("com.project")
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.project")
 public class KioskServiceApplication {
 
     public static void main(String[] args) {
